2eef82ca0d0d7ec939d64fe450e93e1d00c9cee7
Foxrim <Flavien085@gmail.com>
2025-06-04 15:18:43 +0200

    [feat] - connection valide

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { router, rootElement } from './router.tsx';
-import { RouterProvider } from 'react-router';
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { router, rootElement } from "./router.tsx";
+import { RouterProvider } from "react-router";
+import "./index.css";
+import { AuthProvider } from "./pages/Connection/context/Auth.provider.tsx";
 
 if (rootElement == null) {
-    throw new Error(`La route utilisé n'est pas correcte`);
+  throw new Error(`La route utilisé n'est pas correcte`);
 }
 
 createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
-import Auth from "./pages/Auth/Auth";
+import Connection from "./pages/Connection/Connection";
 import NewAccount from "./pages/NewAccount/NewAccount";
+import Home from "./pages/Home/Home";
+import AuthConnection from "./pages/utils/AuthConnection";
 
 export const router = createBrowserRouter([
   {
@@ -10,12 +12,29 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: <Auth />,
+        element: <Connection />,
+      },
+      {
+        path: "login",
+        element: <Connection />
       },
       {
         path: "create-account",
         element: <NewAccount />
-      }
+      },
+
+      // Uniquement connecté
+      {
+        path: "/",
+        element: <AuthConnection />,
+        children: [
+          {
+            path: "home",
+            element: <Home />
+          }
+        ]
+      },
+
     ],
   },
 ]);
