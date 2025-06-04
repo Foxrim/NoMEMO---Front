ede069193349179e777b5d4cd0dd576db06c2ce8
Foxrim <Flavien085@gmail.com>
2025-06-04 16:35:58 +0200

    [feat] - Changement de mot de passe

---
+++ src/pages/utils/TokenVerify.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+export default function TokenVerify() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      const params = new URLSearchParams(window.location.search);
+      const token = params.get("token");
+
+      if (!token) {
+        navigate('/forgot-password');
+        return;
+      }
+
+      try {
+        if (token) {
+          sessionStorage.setItem("resetToken", token);
+
+          navigate(`/new-password?token=${token}`);
+        } else {
+          navigate('/forgot-password');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    verifyToken();
+  }, [navigate]);
+
+  return <p>Vérification du lien en cours...</p>;
+}
