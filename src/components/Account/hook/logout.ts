cebf77b4ee7ae13cd5eb7d1cc1b17ece02d4cba1
Foxrim <Flavien085@gmail.com>
2025-06-05 17:15:01 +0200

    [fix] - modal Compte refermé à la deconnection

--- src/components/Account/hook/logout.ts
+++ src/components/Account/hook/logout.ts
@@ -1,11 +1,14 @@
 import { useAuth } from "../../../pages/Connection/context/useAuth";
 import { useNavigate } from "react-router";
+import { useHeader } from "../../Header/context/useHeader";
 
 function useHandleLogout() {
   const { logout } = useAuth();
   const navigate = useNavigate();
+  const { handleModalAccount } = useHeader();
 
   const handleLogout = () => {
+    handleModalAccount();
     logout();
     navigate("/login");
   };
