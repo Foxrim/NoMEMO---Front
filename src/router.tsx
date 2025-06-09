c90f2bb21028dbd5afb43db199bc0ad2b26ff877
Foxrim <Flavien085@gmail.com>
2025-06-09 17:09:54 +0200

    [feat] - pseudo dans le path

--- src/router.tsx
+++ src/router.tsx
@@ -44,7 +44,7 @@ export const router = createBrowserRouter([
         element: <AuthConnection />,
         children: [
           {
-            path: "home",
+            path: "home/:pseudo",
             element: <Home />
           }
         ]
