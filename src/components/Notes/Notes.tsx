e8f80e70570f31dab17170268fc17a598d7d88b0
Foxrim <Flavien085@gmail.com>
2025-06-11 20:03:56 +0200

    [feat] - Notes stylisable

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -5,12 +5,13 @@ type NotesProps = {
   children: React.ReactNode;
   isDone: string;
   className?: string;
-  onClick?: () => void; 
+  onClick?: () => void;
+  style?: React.CSSProperties;
 };
 
-export default function Notes({ children, isDone, className, onClick }: NotesProps) {
+export default function Notes({ children, isDone, className, onClick, style }: NotesProps) {
   return (
-    <div className={`${styles.notes} ${className}`} onClick={onClick}>
+    <div className={`${styles.notes} ${className}`} onClick={onClick} style={style}>
       <p>{children}</p>
       {isDone === "notDone" ? (
         <img src={xNotDone} alt="croix rouge validé" />
