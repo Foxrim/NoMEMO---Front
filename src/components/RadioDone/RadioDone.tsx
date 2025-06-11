a26b854c4da298d5163b8d4798749f69b83b59ae
Foxrim <Flavien085@gmail.com>
2025-06-11 15:05:22 +0200

    [feat] - visuel Ajout de note

--- src/components/RadioDone/RadioDone.tsx
+++ src/components/RadioDone/RadioDone.tsx
@@ -7,40 +7,54 @@ type RadioDoneProps = {
   setIsDone: React.Dispatch<React.SetStateAction<"done" | "notDone">>;
   isDone: "done" | "notDone";
   className?: string;
-}
+};
 
-export default function RadioDone({ value, setIsDone, isDone, className }: RadioDoneProps) {
-
+export default function RadioDone({
+  value,
+  setIsDone,
+  isDone,
+  className,
+}: RadioDoneProps) {
   return (
-    <div className={`${styles.radioDone} ${className}`}>
-      <input
-        type="radio"
-        name="isDone"
-        id="done"
-        checked={isDone === "done"}
-        onChange={() => setIsDone("done")}
-        value={value}
-      />
-      <i
-        className={`fa-regular fa-square-check ${styles.done} ${
-          isDone === "done" ? styles.doneChecked : ""
-        }`}
-        onClick={() => setIsDone("done")}
-      ></i>
-      <hr />
-      <input
-        type="radio"
-        name="isDone"
-        id="notDone"
-        checked={isDone === "notDone"}
-        onChange={() => setIsDone("notDone")}
-        value={value}
-      />
-      {isDone === "notDone" ? (
-        <img src={xNotDone} alt="croix rouge validé" onClick={() => setIsDone("notDone")} />
-      ) : (
-        <img src={xNotDoneDemi} alt="croix rouge non validé" onClick={() => setIsDone("notDone")} />
-      )}
+    <div className={className}>
+      <div className={styles.radioDone}>
+        <input
+          type="radio"
+          name="isDone"
+          id="done"
+          checked={isDone === "done"}
+          onChange={() => setIsDone("done")}
+          value={value}
+        />
+        <i
+          className={`fa-regular fa-square-check ${styles.done} ${
+            isDone === "done" ? styles.doneChecked : ""
+          }`}
+          onClick={() => setIsDone("done")}
+        ></i>
+        <hr />
+        <input
+          type="radio"
+          name="isDone"
+          id="notDone"
+          checked={isDone === "notDone"}
+          onChange={() => setIsDone("notDone")}
+          value={value}
+        />
+        {isDone === "notDone" ? (
+          <img
+            src={xNotDone}
+            alt="croix rouge validé"
+            onClick={() => setIsDone("notDone")}
+          />
+        ) : (
+          <img
+            src={xNotDoneDemi}
+            alt="croix rouge non validé"
+            onClick={() => setIsDone("notDone")}
+          />
+        )}
+      </div>
     </div>
   );
 }
