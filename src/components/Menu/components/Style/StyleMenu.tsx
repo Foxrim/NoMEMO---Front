c0cac6adcf83203d04c8603be699843f6278aabd
Foxrim <Flavien085@gmail.com>
2025-06-24 18:47:22 +0200

    [feat] - useSort

--- src/components/Menu/components/Style/StyleMenu.tsx
+++ src/components/Menu/components/Style/StyleMenu.tsx
@@ -1,18 +1,20 @@
 import { useModal } from '../../../../contexts/Modal/useModal';
 import useArrangement from '../../hook/arrangement';
+import useSort from '../../hook/sort';
 import OptionStyle from './components/OptionStyle';
 import styles from './StyleMenu.module.css';
 
 export default function StyleMenu() {
   const { handleArrangement } = useModal();
   const { arr } = useArrangement();
+  const { sort } = useSort();
 
   return (
     <div className={styles.styleMenu}>
       <h3>Style</h3>
       <div className={styles.disposalContainer}>
         <OptionStyle onClick={handleArrangement} nameOption={'Disposition'} optionChoose={arr} />
-        <OptionStyle nameOption={'Trier'} optionChoose={'Trier par date de création'} />
+        <OptionStyle nameOption={'Trier'} optionChoose={sort} />
         <OptionStyle nameOption={'Aspect'} optionChoose={'Sombre'} />
       </div>
     </div>
---
+++ src/components/Menu/hook/sort.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useUser } from "../../../contexts/User/useUser";
+
+function useSort() {
+    const [sort, setSort] = useState<string>('');
+
+    const sorts = ['Trier par date de création', 'Trier par date de modification'];
+    const sortValues = ['creation', 'modify'];
+
+    const { user, fetchUser } = useUser();
+
+    useEffect(() => {
+        if (user.sort === 'creation') {
+            setSort('Trier par date de création');
+        } else if (user.sort === 'modify') {
+            setSort('Trier par date de modification');
+        } else {
+            setSort('non trouvé');
+        }
+    }, [user, setSort]);
+
+    const handleUpdateSort = async (index: number) => {
+        const selected = sortValues[index];
+
+        try {
+            const res = await fetch ('http://localhost:5012/api/v1/users/modidy-me', {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ sort: selected }),
+                credentials: 'include',
+            });
+
+            if (res.ok) {
+                fetchUser();
+            }
+
+        }  catch (err) {
+            console.error('Une erreur est survenue: ', err);
+        }
+    }
+
+
+
+    return { sorts, sort, handleUpdateSort };
+}
+
+export default useSort;
