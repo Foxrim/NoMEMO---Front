1aeae1d56b96d8c0f3ede9a22e58b34f5c7d6a4b
Foxrim <Flavien085@gmail.com>
2025-06-25 14:41:12 +0200

    [fix] - balise en trop

--- src/components/Notes Container/components/Notes/Notes.tsx
+++ src/components/Notes Container/components/Notes/Notes.tsx
@@ -2,12 +2,10 @@ import styles from "./Notes.module.css";
 
 export default function Notes() {
   return (
-    <>
       <div className={styles.notes}>
         <h3>Titre</h3>
         <p>Commentaire</p>
         <hr className={styles.hrNotes} />
       </div>
-    </>
   );
 }
