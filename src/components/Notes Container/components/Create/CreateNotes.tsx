7834b641c2a395c80aaa08fe33a5a2766e335374
Foxrim <Flavien085@gmail.com>
2025-06-25 15:49:17 +0200

    [update] - options composant réutilisable

--- src/components/Notes Container/components/Create/CreateNotes.tsx
+++ src/components/Notes Container/components/Create/CreateNotes.tsx
@@ -1,4 +1,5 @@
 import FormModal from "../../../Modals/Form Modal/FormModal";
+import Options from "../../../Options/Options";
 
 export default function CreateNotes() {
     return (
@@ -6,7 +7,11 @@ export default function CreateNotes() {
             <input type="text" name="" id="" />
             <hr />
             <textarea name="" id=""></textarea>
-
+            <hr />
+            <Options nameOption="Catégorie" optionChoose="Aucune" />
+            <Options nameOption="Status" optionChoose="Aucun" />
+            <hr />
+            <input type="text" />
         </FormModal>
     )
 }
